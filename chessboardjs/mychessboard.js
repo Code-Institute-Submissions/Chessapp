fcc9a3c21d46e84405dea93c2898f5d92342e608
rbnphlp <rbnphlp@hotmail.com>
2020-04-22 18:27:21 +0000

    Need to find correct  Chess engine and Backend classes

--- chessboardjs/mychessboard.js
+++ chessboardjs/mychessboard.js
@@ -1,1 +1,61 @@
-var board1 = ChessBoard('board1', 'start');
+// Use Library Default to make draggable
+
+var config = {
+  draggable: true,
+  dropOffBoard: 'snapback', // this is the default
+  position: 'start'
+}
+
+
+
+// Initial empty chess board with  pieces 
+
+var board1 = ChessBoard('board1', config);
+
+
+// Interact with pieces and change positions after dragging and dropping
+
+
+
+
+
+/** CHeck rules for chess engine*/
+
+class Checkmate{
+// Workout if the check mate 
+
+
+
+};
+
+
+
+class  is_check{
+// is the piece under check.
+
+
+}
+
+
+/** Legal Valid moves */
+
+// Two ways to describe moves  in chess : Descriptive and Algeabric
+
+
+
+
+class Piecemoves{
+// Class to work out possible moves for each chess -pawn move 1 forawrd , knight L etc
+
+
+
+};
+
+
+/** PGN Parser for chess board can interpret */
+
+
+
+
+
+/*** Parse moves from computer  (which engine ?)--- Check if this is possible */
