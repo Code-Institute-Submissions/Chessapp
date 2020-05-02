1b8fc7b2606cff6644fbe422fa0675a52c09041b
rbnphlp <rbnphlp@hotmail.com>
2020-05-02 15:47:12 +0000

    Added chess.js lib

--- chessboardjs/mychessboard.js
+++ chessboardjs/mychessboard.js
@@ -35,12 +35,15 @@ console.log(board1.fen());
 
 $('#board1').on('click',clickShowPositionBtn)
 
+///////////** Require chess js module */
 
 
-
-
-
-
-
+const chess = new Chess()
+while (!chess.game_over()) {
+    const moves = chess.moves()
+    const move = moves[Math.floor(Math.random() * moves.length)]
+    chess.move(move)
+}
+console.log(chess.pgn())
 
 
