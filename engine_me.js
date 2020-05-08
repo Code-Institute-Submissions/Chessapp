4540938409708cfd4fef17f9b9862bdb84453ead
Robin Philip <rbnphlp@hotmail.com>
2020-05-08 18:20:21 +0000

    Added ondrag fun to pick up pieces (not tested)

--- engine_me.js
+++ engine_me.js
@@ -1,7 +1,12 @@
+// Initiate Game Variables ::
+
+
+var piece_colour="w";
+
+
 
 
 //Initiate engine and Chess js library 
-
 
 
 //  create a new chess constructor from the chessJs lib
@@ -18,4 +23,51 @@
  stockfish_engine.onmessage = function(event) {
   console.log(event.data);
 };
- console.log("Engine output" +message_engine)
+
+
+
+
+///  Define generic functions  to get  moves played by the user and posting it to engine:
+
+
+
+// Function from chessbaord library on when to pick pieces
+function onDragStart (source, piece, position, orientation) {
+  
+
+  // test my arrow skills
+    const piece_colour_func =(piece_colour) =>(piece_colour=="w")?  /^b/ : /^w/
+
+    // At the start of game: do not pick up pieces if the game is over or the wrong colour
+  if (chess_game.game_over() || piece.search(piece_colour_func(piece_colour))!== -1){
+
+    return false;
+
+  }
+
+
+}
+
+
+ 
+
+var config = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+}
+
+
+myboard = new ChessBoard('board', config);
+
+
+
+function moves_by_user(){
+
+
+
+
+
+}
